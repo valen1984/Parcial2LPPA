e070fa4d7fab228c4b0ea8087b56f33b42ffd080
Valentin <valen1984@gmail.com>
2022-07-04 12:01:45 -0300

    Chequeo de credenciales y modal

--- JS/login.js
+++ JS/login.js
@@ -38,3 +38,72 @@ function validateCredentials(credentials) {
       console.log(error);
     });
 }
+function validateCredentialsOnSubmit(e) {
+  e.preventDefault();
+  const credentials = {
+    email: email.value,
+    pass: pass.value,
+  };
+  validateCredentials(credentials)
+    .then(function(successStatus) {
+      if (successStatus) {
+        saveCredentialsToLocalStorage(credentials)
+        window.location.assign('/dashboard.html')
+      } else {
+       // alert("Usuario incorrecto");
+        modal.style.display = "block";
+       // console.log(successStatus, "todo mal");
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
+
+function checkForLoginCredentialsInLocalStorage() {
+  return localStorage.getItem("email") && localStorage.getItem("pass");
+}
+
+if (checkForLoginCredentialsInLocalStorage()) {
+    const credentials = {
+      email: localStorage.getItem("email"),
+      pass: localStorage.getItem("pass"),
+    };
+
+    validateCredentials(credentials)
+      .then(function(successStatus) {
+        if (successStatus) {
+          window.location.assign('/dashboard.html')
+        } else {
+          window.location.assign('/index.html')
+        }
+      })
+      .catch(function(error) {
+        console.log(error);
+      })
+} else   {
+  if (location.origin == '/dashboard.html'){
+    window.location.assign('/index.html')
+  }
+}
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("submit");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
