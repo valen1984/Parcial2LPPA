d3b4e91d0fd67dc6293056bd6ab8ca84ab043d10
Valentin <valen1984@gmail.com>
2022-07-05 01:07:13 -0300

    Update table.js Modal web could not load

--- JS/table.js
+++ JS/table.js
@@ -72,6 +72,30 @@ fetch('https://basic-server-one.vercel.app/users')
 .then(function(respuesta){
     return respuesta.json();
 })
+
+ // Get the modal
+    const modal = document.getElementById("myModal");
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks the button, open the modal 
+    span.onclick = function() {
+        modal.style.display = "block";
+    }
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
 .then(function(respuestaJson){
     tabla(respuestaJson);;
 });
